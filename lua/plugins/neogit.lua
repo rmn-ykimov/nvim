27db5deb23680cff57169dcf47c0967fa3b3e69e
Roman Yakimov <{ID}+{username}@users.noreply.github.com>
2025-01-05 17:29:19 +0100

    add modularity to config

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,49 @@
+return {
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"sindrets/diffview.nvim",
+			"nvim-telescope/telescope.nvim",
+			-- 'ibhagwan/fzf-lua',
+		},
+		config = function()
+			local neogit = require("neogit")
+
+			neogit.setup({})
+
+			-- Keymap settings
+			vim.keymap.set(
+				"n",
+				"<leader>gs",
+				neogit.open,
+				{ silent = true, noremap = true, desc = "Open Neogit status" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>gc",
+				":Neogit commit<CR>",
+				{ silent = true, noremap = true, desc = "Neogit commit" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>gp",
+				":Neogit pull<CR>",
+				{ silent = true, noremap = true, desc = "Neogit pull" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>gP",
+				":Neogit push<CR>",
+				{ silent = true, noremap = true, desc = "Neogit push" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>gb",
+				":Telescope git_branches<CR>",
+				{ silent = true, noremap = true, desc = "Telescope git branches" }
+			)
+			vim.keymap.set("n", "<leader>gB", ":G blame<CR>", { silent = true, noremap = true, desc = "Git blame" })
+		end,
+	},
+}
---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,112 @@
+return {
+	"epwalsh/obsidian.nvim",
+	version = "*", -- recommended, use latest release instead of latest commit
+	lazy = true,
+	ft = "markdown",
+	-- Replace the above line with this if you only want to load obsidian.nvim
+	-- for markdown files in your vault:
+	-- event = {
+	--   -- If you want to use the home shortcut '~' here you need to call
+	--   'vim.fn.expand'.
+	--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+	--   "BufReadPre path/to/my-vault/**.md",
+	--   "BufNewFile path/to/my-vault/**.md",
+	-- },
+	dependencies = {
+		-- Required.
+		"nvim-lua/plenary.nvim",
+	},
+
+	opts = {
+
+		workspaces = {
+			{
+				name = "personal",
+				path = "~/zettel",
+			},
+		},
+
+		templates = {
+			folder = "Templates",
+			date_format = "%Y-%m-%d",
+			time_format = "%H:%M",
+			-- A map for custom variables, the key should be the variable and the
+			-- value a function
+			substitutions = {},
+		},
+
+		-- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
+		completion = {
+			-- Set to false to disable completion.
+			nvim_cmp = true,
+			-- Trigger completion at 2 chars.
+			min_chars = 2,
+		},
+
+		-- Optional, configure key mappings. These are the defaults. If you don't want to set any keymappings this
+		-- way then set 'mappings = {}'.
+		mappings = {
+			-- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+			["gf"] = {
+				action = function()
+					return require("obsidian").util.gf_passthrough()
+				end,
+				opts = { noremap = false, expr = true, buffer = true },
+			},
+			-- Toggle check-boxes.
+			["<leader>ch"] = {
+				action = function()
+					return require("obsidian").util.toggle_checkbox()
+				end,
+				opts = { buffer = true },
+			},
+			-- Smart action depending on context, either follow link or toggle checkbox.
+			["<cr>"] = {
+				action = function()
+					return require("obsidian").util.smart_action()
+				end,
+				opts = { buffer = true, expr = true },
+			},
+		},
+
+		new_notes_location = "0. Inbox",
+
+		-- Optional, sort search results by "path", "modified", "accessed", or "created".
+		-- The recommend value is "modified" and `true` for `sort_reversed`, which means, for example,
+		-- that `:ObsidianQuickSwitch` will show the notes sorted by latest modified time
+		sort_by = "modified",
+		sort_reversed = true,
+
+		-- Optional, determines how certain commands open notes. The valid options are:
+		-- 1. "current" (the default) - to always open in the current window
+		-- 2. "vsplit" - to open in a vertical split if there's not already a vertical split
+		-- 3. "hsplit" - to open in a horizontal split if there's not already a horizontal split
+		open_notes_in = "current",
+
+		-- Specify how to handle attachments.
+		attachments = {
+			-- The default folder to place images in via `:ObsidianPasteImg`.
+			-- If this is a relative path it will be interpreted as relative to the vault root.
+			-- You can always override this per image by passing a full path to the command instead of just a filename.
+			img_folder = "assets/imgs", -- This is the default
+
+			-- Optional, customize the default name or prefix when pasting images via `:ObsidianPasteImg`.
+			---@return string
+			img_name_func = function()
+				-- Prefix image names with timestamp.
+				return string.format("%s-", os.time())
+			end,
+
+			-- A function that determines the text to insert in the note when pasting an image.
+			-- It takes two arguments, the `obsidian.Client` and an `obsidian.Path` to the image file.
+			-- This is the default implementation.
+			---@param client obsidian.Client
+			---@param path obsidian.Path the absolute path to the image file
+			---@return string
+			img_text_func = function(client, path)
+				path = client:vault_relative_path(path) or path
+				return string.format("![%s](%s)", path.name, path)
+			end,
+		},
+	},
+}
