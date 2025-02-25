90ae92499d81e57582d7c2d2b0d3b2bf84879d5f
Roman Yakimov <85025748+rmn-ykimov@users.noreply.github.com>
2025-02-25 04:58:27 -0800

    change disable frontmatter to true (obsidian.nvim), add jj keymap

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -35,6 +35,8 @@ return {
 			substitutions = {},
 		},
 
+		disable_frontmatter = true,
+
 		-- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
 		completion = {
 			-- Set to false to disable completion.
