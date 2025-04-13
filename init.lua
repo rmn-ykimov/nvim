0252cd7b3bfb9e666225ff50ddc874167bb1743d
Roman Yakimov <{ID}+{username}@users.noreply.github.com>
2025-04-13 11:33:05 +0200

    Move lazy into a plugin-manager directory.

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ require("core.options")
 require("core.keymaps")
 require("core.autocommands")
 
-require("plugins.lazy").setup()
+require("plugin-manager.lazy").setup()
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
