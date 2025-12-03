6a96099b18a8e452b9d9eff7800b412a694e78c5
Roman Yakimov <85025748+rmn-ykimov@users.noreply.github.com>
2025-12-03 18:01:33 +0100

    Remove lazy option for Obsidian plugin.

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -28,7 +28,8 @@ vim.opt.breakindent = true
 -- Save undo history
 vim.opt.undofile = true
 
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the
+-- search term
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
